1482e97298332722ae2a8990e0dc4c10bc3b7a68
starr9622 <sbseodev@gmail.com>
2020-06-06 17:47:58 +0900

    refactor :: 캐러샐 동작 수정

--- carousel/src/components/Carousel.vue
+++ carousel/src/components/Carousel.vue
@@ -1,27 +1,28 @@
 <template>
   <div>
     <h3>Carousel</h3>
-    <div class="carousel">
-      <ul>
-        <li v-for="(item, index) in imgdata" :key="index" :id="'imgWrap'+item.id" 
-		class="imglist"
-		>
-          <img :src="item.imageUrl" />
-        </li>
-      </ul>
-    </div>
-
-	<input type="button" class="button-prev" @click="moveImgAction('prev')" value="<" />
-      <span>{{select.title}}</span>
-      <input type="button" class="button-next" @click="moveImgAction()" value=">" />
-    <h3>이미지 삭제</h3>
-    <div>
-      <label>
-        id:
-        <input type="number" v-model="deleteId" />
-      </label>
-      <button @click="delectAction()">삭제</button>
-    </div>
+	<main class="carousel">
+		<div class="imglist">
+			<div class="item" v-for="(item, index) in imgdata" :key="index" :id="'imgWrap'+item.id">
+				<img :src="item.imageUrl" />
+			</div>
+		</div>
+	</main>
+	<nav>
+		<input type="button" class="button-prev" @click="moveImgAction('prev')" value="<" />
+		<span>{{select.title}}</span>
+		<input type="button" class="button-next" @click="moveImgAction()" value=">" />
+	</nav>
+	<section>
+		<h3>이미지 삭제</h3>
+		<div>
+			<label>
+				id:
+				<input type="number" v-model="deleteId" />
+			</label>
+			<button @click="delectAction()">삭제</button>
+		</div>
+	</section>
   </div>
 </template>
 
@@ -32,39 +33,43 @@ export default {
 			imgdata: require('../assets/data.json'),
 			select: {},
 			deleteId: '',
+			speed: 500,
+			timer: ''
 		};
 	},
 	mounted() {
-		this.init(0);
+		this.loof(0);
 	},
 	methods: {
-		init(setIndex) {
-			this.select = this.imgdata[setIndex];
-			setTimeout(this.moveImgAction, 5000);
+		loof(index){
+			this.select = this.imgdata[index];
+			this.timer = setTimeout(this.moveImgAction, 5000);	
 		},
-		moveImgAction(key) {
+		moveImgAction(key){
+			clearTimeout(this.timer);
 			let nowindex = this.imgdata.indexOf(this.select);
-			let x = document.querySelector(".carousel").scrollLeft;
-			let changeIndex =
-				key === 'prev' && nowindex - 1 >= 0
-					? nowindex - 1
-					: (changeIndex =
-							key === 'prev' && nowindex - 1 < 0
-								? this.imgdata.length - 1
-								: (changeIndex =
-										nowindex + 1 <= this.imgdata.length - 1
-											? nowindex + 1
-											: 0));
-			let left = changeIndex > nowindex ? x+400 : changeIndex === 0 ? 0 : x-400; 
-			document.querySelector(".carousel").scroll({
-				left: left,  
-      			behavior: 'smooth'});
-			this.init(changeIndex);
-			
+			let nextIndex = nowindex + 1;
+			if(key == "prev") nextIndex = nowindex - 1;
+			if(nextIndex < 0) nextIndex = this.imgdata.length-1;
+			if(nextIndex > this.imgdata.length-1) nextIndex = 0;
+			let list = document.querySelector(".imglist");
+			list.style.transition = this.speed+"ms";
+			list.style.transform = "translate(-"+(400 * nextIndex)+"px, 0)";
+
+			this.loof(nextIndex);
 		},
 		delectAction() {
 			const delIndex = this.imgdata.findIndex(img => img.id == this.deleteId);
-			delIndex > -1 ? this.imgdata.splice(delIndex, 1) : '';
+			if(delIndex > -1){
+				if(this.imgdata.indexOf(this.select) == delIndex){
+					let nextIndx = delIndex+1;
+					if(nextIndx > this.imgdata.length-1) nextIndx = 0;
+					this.loof(nextIndx);
+				}
+				this.imgdata.splice(delIndex, 1);
+			}else{
+				alert("해당하는 이미지 번호는 존재 하지않습니다.");
+			}
 			this.deleteId = '';
 		},
 	},
@@ -74,30 +79,14 @@ export default {
 <style scoped>
 .carousel{
 	width: 400px;
-	overflow: scroll;
-	-ms-overflow-style: none;
-}
-.carousel::-webkit-scrollbar{
-	display: none;
-}
-.carousel ul {
-	margin: 0;
-	padding: 0;
-	width: max-content;
-	height: 300px;
 	overflow: hidden;
+	position: relative;
+	margin: auto;
+}
+.imglist{
 	display: flex;
 }
-.carousel ul li{
-	list-style: none;
-	margin: 0 0 0 0;
-    padding: 0 0 0 0;
-    border : 0;
-    float: left;
-}
-.carousel ul li img{
-	object-fit: cover;
-	width: 100%;
-	height: 100%;
+nav{
+	text-align: center;
 }
 </style>
